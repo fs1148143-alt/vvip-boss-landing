38add84d17aabadd2060e460817eca1bb58dc1ea
fs1148143-alt <fs1148143@gmail.com>
2026-02-02 20:43:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     let current = 0;
     const track = document.getElementById('slider');
-    
-    function slide() {
+    setInterval(() => {
         current = (current + 1) % 5;
         track.style.transform = translateX(-${current * 20}%);
-    }
-
-    // Har 3.5 second mein slide change hogi
-    setInterval(slide, 3500);
+    }, 3500);
 });
