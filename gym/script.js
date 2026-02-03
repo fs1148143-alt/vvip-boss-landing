5fe5989f47d5d008bbed52546607d2107c882d80
fs1148143-alt <fs1148143@gmail.com>
2026-02-03 18:09:56 +0530

    Update script.js

--- gym/script.js
+++ gym/script.js
@@ -1,20 +1,21 @@
 gsap.registerPlugin(ScrollTrigger);
 
-let sections = gsap.utils.toArray(".panel");
+let panels = gsap.utils.toArray(".panel");
 
-gsap.to(sections, {
-    xPercent: -100 * (sections.length - 1),
+gsap.to(panels, {
+    xPercent: -100 * (panels.length - 1),
     ease: "none",
     scrollTrigger: {
         trigger: ".wrapper",
         pin: true,
         scrub: 1,
-        end: "+=2000"
+        end: "+=3000"
     }
 });
 
-gsap.timeline({
-    scrollTrigger: { trigger: ".wrapper", scrub: 1, start: "top top", end: "bottom bottom" }
-})
-.to("#db-pro", { rotation: 360, x: "-25vw", scale: 0.7 })
-.to("#db-pro", { rotation: 720, x: "25vw", y: "-10vh", scale: 1 });
+gsap.to("#db-pro", {
+    scrollTrigger: { trigger: ".wrapper", scrub: 1, start: "top top", end: "bottom bottom" },
+    rotation: 360,
+    x: "20vw",
+    scale: 0.8
+});
